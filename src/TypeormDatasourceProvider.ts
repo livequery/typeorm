7be9824d7e6876847904d4c8d25203545b5d0b81
Ba <duongvanba.com@gmail.com>
2022-11-24 21:09:23 +0700

    Update

--- src/TypeormDatasourceProvider.ts
+++ src/TypeormDatasourceProvider.ts
@@ -5,28 +5,28 @@ import { DataSource } from 'typeorm'
 import { getConnectionToken } from '@nestjs/typeorm'
 import { getEntityName } from "./helpers/getEntityName";
 
-const [_, UseTypeormDatasource] = createDatasourceMapper<RouteOptions>(TypeormDatasource)
+export const [UseTypeormDatasource, getMetadatas] = createDatasourceMapper<RouteOptions>(TypeormDatasource)
 
-export const TypeormDatasourceProviderWithMultipleConnections = (connection_names: string[] = []) => _(options => {
 
-    const options_with_name = options.map(option => ({
-        ...option,
-        collection_name: getEntityName(option.entity)
-    }))
+export const TypeormDatasourceProviderWithMultipleConnections = (connection_names: string[] = []) => {
 
     return {
         provide: TypeormDatasource,
         inject: [undefined, ...connection_names].map(connection_name => getConnectionToken(connection_name)),
-        useFactory: (...connections: DataSource[]) => new TypeormDatasource(
-            connections,
-            options_with_name
-        )
+        useFactory: (...connections: DataSource[]) => {
+
+            const options_with_name = getMetadatas().map(option => ({
+                ...option,
+                collection_name: getEntityName(option.entity)
+            }))
+
+            return new TypeormDatasource(
+                connections,
+                options_with_name
+            )
+        }
     }
-})
-
-export { UseTypeormDatasource }
-
-export const TypeormDatasourceProvider = {
-    provide: TypeormDatasource,
-    useFactory: () => TypeormDatasourceProviderWithMultipleConnections()
 }
+
+
+export const TypeormDatasourceProvider = TypeormDatasourceProviderWithMultipleConnections() 
