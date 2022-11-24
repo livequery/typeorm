1a453d348d21b1842f1674003fd893235190f822
Ba <duongvanba.com@gmail.com>
2022-11-24 21:36:52 +0700

    Update

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+export { getEntityName } from './src/helpers/getEntityName'
 export { TypeormDatasource } from './src/TypeormDatasource'
 export * from './src/DataChangePayload'
 export * from './src/RouteOptions'
