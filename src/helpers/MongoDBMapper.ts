2ac3f11deb7168cbe1b2074a872567dc05a70082
Ba <duongvanba.com@gmail.com>
2023-03-03 15:51:26 +0700

    Update _id

--- src/helpers/MongoDBMapper.ts
+++ src/helpers/MongoDBMapper.ts
@@ -1,11 +1,15 @@
+const { ObjectID } = require('mongodb')
+
 export class MongoDBMapper {
 
     static toMongoDBObject<T extends { [key: string]: any }>(data: T) {
+        if (!data) return data
         const { id, _id, ...rest } = data;
-        return id ? { _id: id, ...rest } : data
+        return id ? { _id: ObjectID(id), ...rest } : data
     }
 
     static fromMongoDBObject<T extends { [key: string]: any }>(data: any) {
+        if (!data) return data
         const { _id, id, ...rest } = data;
         return _id ? { id: _id, ...rest } : data
     }
