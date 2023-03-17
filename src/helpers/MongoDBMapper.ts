159d5247ec3d8c0e6c8d87b894519069dd4f73da
Ba <duongvanba.com@gmail.com>
2023-03-17 15:14:03 +0700

    Update toMongoDBQuery

--- src/helpers/MongoDBMapper.ts
+++ src/helpers/MongoDBMapper.ts
@@ -1,20 +1,30 @@
-const { ObjectID } = require('mongodb')
+const { ObjectId } = require('mongodb')
 
 export class MongoDBMapper {
 
     static toMongoDBObject<T extends { [key: string]: any }>(data: T) {
         if (!data) return data
         const { id, _id, ...rest } = data;
-        return id ? { _id: ObjectID(id), ...rest } : data
+        return id ? { _id: ObjectId(id), ...rest } : data
     }
 
     static toMongoDBQuery<T extends { [key: string]: any }>(data: T) {
-        if (!data) return data
+        if (!data || !data.id) return data
         const { id, _id, ...rest } = data;
-        return id ? { _id: id, ...rest } : data
+        if (typeof id == 'string') return { _id: new ObjectId(id), ...rest }
+        if (typeof id == 'object') return {
+            _id: Object.entries(id).reduce((p, [key, value]) => ({
+                ...p,
+                [key]: Array.isArray(value) ? value.map(v => new ObjectId(v)) : (
+                    typeof value == 'string' ? new ObjectId(value) : value
+                )
+            }), {}),
+            ...rest
+        }
+        return { _id: id, ...rest }
     }
 
-    static fromMongoDBObject<T extends { [key: string]: any }>(data: any) {
+    static fromMongoDBObject(data: any) {
         if (!data) return data
         const { _id, id, ...rest } = data;
         return _id ? { id: _id, ...rest } : data
