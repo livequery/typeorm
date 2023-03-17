7140eb35b9bb77b23a01ddc30d80b5286570a258
Ba <duongvanba.com@gmail.com>
2023-03-17 15:01:58 +0700

    Update

--- src/helpers/MongoDBMapper.ts
+++ src/helpers/MongoDBMapper.ts
@@ -8,6 +8,12 @@ export class MongoDBMapper {
         return id ? { _id: ObjectID(id), ...rest } : data
     }
 
+    static toMongoDBQuery<T extends { [key: string]: any }>(data: T) {
+        if (!data) return data
+        const { id, _id, ...rest } = data;
+        return id ? { _id: id, ...rest } : data
+    }
+
     static fromMongoDBObject<T extends { [key: string]: any }>(data: any) {
         if (!data) return data
         const { _id, id, ...rest } = data;
